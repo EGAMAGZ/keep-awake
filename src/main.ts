c67751792e3c00ffc56384a9841e530ead549db7
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-05-10 21:43:50 -0600

    fix: Real fix to js executing

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,4 @@ import "./styles.css";
 import van from "vanjs-core";
 import App from "./app";
 
-const Main = () => {
-    van.add(document.getElementById("app")!, App);
-};
-
-export default Main;
+van.add(document.getElementById("app")!, App);
